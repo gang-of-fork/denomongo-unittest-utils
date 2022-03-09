a5245920efdbfe422c4612c4badce517ac77b7ec
lumaghg <rotkehlchen567@gmail.com>
2022-03-09 20:49:50 +0100

    cleanup implementing reset option for Collection Mocks

--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,3 @@
 export type { Filter, CountOptions } from "./deps.ts"
-export { assertSpyCall, Spy, assertSpyCallAsync } from "https://deno.land/x/mock@0.13.0/mod.ts"
+export { assertSpyCall, assertSpyCallAsync } from "https://deno.land/x/mock@0.13.0/mod.ts"
+export type  { Spy } from "https://deno.land/x/mock@0.13.0/mod.ts"
